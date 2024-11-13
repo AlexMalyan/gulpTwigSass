dd11611a6d22349da2ef1d36529acb8bfee1dfbd
admin <rostelecoom55@gmail.com>
2024-11-13 10:49:02 +0300

    Перенес stocks about breacrumbs на twig

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,20 @@
-
-const { series, parallel } = require('gulp');
-const requireDir = require('require-dir');
-const tasks = requireDir('./tasks');
+const { series, parallel } = require("gulp");
+const requireDir = require("require-dir");
+const tasks = requireDir("./tasks");
 
 // console.log(tasks.copyResources);
 
 // exports.fonts = tasks.copyResources.copyFonts
-exports.svg = tasks.copyResources.copySvg
+exports.svg = tasks.copyResources.copySvg;
 
 // // exports.test = tasks.test
 // exports.clean = tasks.clean;
 // // exports.copyImages = tasks.copyImages
 // exports.scripts = tasks.scripts
-// exports.twig = tasks.twig
+exports.twig = tasks.twig;
 // exports.styles = tasks.styles
 // // exports.browserSync = tasks.browsersync
-exports.watch = tasks.watcher
+exports.watch = tasks.watcher;
 
 exports.default = parallel(
   tasks.copyResources.copyResources,
@@ -23,7 +22,7 @@ exports.default = parallel(
   tasks.scripts,
   tasks.twig,
   tasks.watcher
-)
+);
 // exports.build = series(
 //   'clean',
 //   'copyImages',
--- tasks/twig.js
+++ tasks/twig.js
@@ -1,24 +1,28 @@
+const { src, dest } = require("gulp");
+const data = require("gulp-data");
+const twig = require("gulp-twig");
 
-const { src, dest } = require('gulp');
-const data = require('gulp-data');
-const twig = require('gulp-twig');
-
-const { twigSrc, twigDataDir } = require('./vars');
-const path = require('path');
-const fs = require('fs');
+const { twigSrc, twigDataDir } = require("./vars");
+const path = require("path");
+const fs = require("fs");
+const { log } = require("console");
 
 module.exports = function twigs() {
-    return src(twigSrc)
-        .pipe(data(function (file) {
-            const result = {};
-            const files = fs.readdirSync(twigDataDir);
+  return src(twigSrc)
+    .pipe(
+      data(function (file) {
+        const result = {};
+        const files = fs.readdirSync(twigDataDir);
 
-            for (let file of files) {
-                result[path.basename(file, '.json')] = JSON.parse(fs.readFileSync(twigDataDir + '/' + file).toString())
-            }
-            return result
-        }))
-        .pipe(twig())
-        .pipe(dest('./dist/'))
+        for (let file of files) {
+          result[path.basename(file, ".json")] = JSON.parse(
+            fs.readFileSync(twigDataDir + "/" + file).toString()
+          );
+        }
 
-}
+        return result;
+      })
+    )
+    .pipe(twig())
+    .pipe(dest("./dist/"));
+};
