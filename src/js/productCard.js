fe0e07f72dce6276495c303c390c37ede970360a
admin <rostelecoom55@gmail.com>
2024-11-17 22:06:31 +0300

    Улучшил твою простыню в product.twig и поднял свой лвл в понимании этого шаблонизатора

--- src/js/productCard.js
+++ src/js/productCard.js
@@ -1,32 +1,40 @@
-const items = document.querySelectorAll('.product__menu-item');
-const item = document.querySelector('.product__menu-item');
-const icon = item.querySelector('.icon');
+const items = document.querySelectorAll(".product__menu-item");
+const item = document.querySelector(".product__menu-item");
+const icon = item.querySelector(".icon");
 
-if (item.classList.contains('product__menu-item--has-sub')) {
-  icon.classList.add('product__menu-icon--active');
+if (item.classList.contains("product__menu-item--has-sub")) {
+  icon.classList.add("product__menu-icon--active");
 }
 
 // =include productCardMenu.js
-const element = document.querySelector('.product__main-share-button');
-const block = document.querySelector('.product__main-share-block');
+const element = document.querySelector(".product__main-share-button");
+const block = document.querySelector(".product__main-share-block");
 
-const dialogElement = document.querySelector('.buy-one-click');
-const openBtn = document.querySelector('.product__main-oneclick');
+element.addEventListener("click", function () {
+  if (block.classList.contains("product__main-share-block--open")) {
+    block.classList.remove("product__main-share-block--open");
+  } else {
+    block.classList.add("product__main-share-block--open");
+  }
+});
+
+const dialogElement = document.querySelector(".buy-one-click");
+const openBtn = document.querySelector(".product__main-oneclick");
 const closeBtn = document.querySelector(".buy-one-click__btn-cancel");
 
 /* desription */
-const description = document.querySelector('.product__description');
-const descBtn = document.querySelector('.product__main-more');
-document.querySelector('.product__main-more').addEventListener('click', () => {
+const description = document.querySelector(".product__description");
+const descBtn = document.querySelector(".product__main-more");
+document.querySelector(".product__main-more").addEventListener("click", () => {
+  description.classList.toggle("product__description--is-active");
 
-  description.classList.toggle('product__description--is-active');
-
-  if (description.classList.contains('product__description--is-active')) {
-    document.querySelector('.product__main-more').textContent = 'Скрыть';
+  if (description.classList.contains("product__description--is-active")) {
+    document.querySelector(".product__main-more").textContent = "Скрыть";
   } else {
-    document.querySelector('.product__main-more').textContent = 'Все характеристики';
+    document.querySelector(".product__main-more").textContent =
+      "Все характеристики";
   }
-})
+});
 
 openBtn.addEventListener("click", () => {
   dialogElement.showModal();
@@ -36,16 +44,13 @@ closeBtn.addEventListener("click", () => {
   dialogElement.close();
 });
 
-element.addEventListener('click', () => {
-  if (block.classList.contains('is-active')) {
-    block.classList.remove('is-active');
+element.addEventListener("click", () => {
+  if (block.classList.contains("is-active")) {
+    block.classList.remove("is-active");
     return;
-  }
-  else {
-    block.classList.add('is-active');
+  } else {
+    block.classList.add("is-active");
   }
 
   // element.classList.toggle('is-active');
 });
-
-
